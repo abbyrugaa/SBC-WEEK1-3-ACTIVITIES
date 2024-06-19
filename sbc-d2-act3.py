75ffe29a093446c628b2bd261e98779976f9dc1e
abbyrugaa <abby.gaile.ruga@aclcbutuan.edu.ph>
2024-06-19 23:20:54 +0800

    version 1

---
+++ sbc-d2-act3.py
@@ -0,0 +1,19 @@
+from random import randint
+
+oc = ["1", "2", "3", "5"]
+
+b = input("Enter your bet (1 2 3): ")
+
+res = "".join([oc[randint(0, 3)] for _ in range(3)])
+
+if res == "123":
+    out = "You Win!"
+elif res[0] == "2" and res[1] == "1" and res[2] == "3":
+    out = "You Partially Win!"
+elif res == "555":
+    out = "You lose!"
+else:
+    out = "No special outcome."
+
+print(f"Result = {res}")
+print(out)
